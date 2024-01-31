cd81e21bac607e671d0f6934b004aaa93fc27895
oruban <oruban@student.42wolfsburg.de>
2024-01-31 21:31:38 +0100

    push() statrted

--- slarge_utils.c
+++ slarge_utils.c
@@ -6,18 +6,32 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 09:48:03 by oruban            #+#    #+#             */
-/*   Updated: 2024/01/31 09:53:52 by oruban           ###   ########.fr       */
+/*   Updated: 2024/01/31 21:31:11 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+
+void	push(t_stack **src, t_stack **dest)
+{
+	if (!*src)
+		return ;
+}
+
 void	sort_large_stack(t_stack **a)
 {
 	size_t		size;
+	t_stack		*b;
 
+	b = NULL;
 	size = 0;
 	size = stack_size(*a);
+	if (size > 3 && !issorted(*a))
+	{
+		push(a, &b);
+		ft_printf("pb\n", (int) size);
+		size--;
+	}
 	
-	ft_printf("stack size of %s\n", size);
 }
